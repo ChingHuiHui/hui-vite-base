a0f6d5f988f607cb703f1f52f8700fa81a6c6997
chinghuihui <sunny70624@gmail.com>
2022-01-09 17:14:18 +0800

    init (eslint, prettier, tailwindcss)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // content: Add the paths to all of your template files in your tailwind.config.js file.
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
