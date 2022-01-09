702fde505822dd1fcca3555b9b4af2112fe749af
chinghuihui <sunny70624@gmail.com>
2022-01-09 22:01:48 +0800

    add fontawesome & create HuiIcon component

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import { createPinia } from 'pinia'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import './assets/styles/index.css'
 
 const app = createApp(App)
 const pinia = createPinia()
+
+const fontAwesomePackages = [fas, fab, far]
+
+function loadFontAwesome() {
+  fontAwesomePackages.forEach((pack) => library.add(pack))
+
+  app.component('font-awesome-icon', FontAwesomeIcon)
+}
+
+loadFontAwesome()
 
 app.use(pinia).mount('#app')
