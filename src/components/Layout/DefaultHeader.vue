e9955eeb5c6e1b526fd46e824459a46d4444102c
chinghuihui <sunny70624@gmail.com>
2022-01-15 23:51:16 +0800

    add locale underline style

--- src/components/Layout/DefaultHeader.vue
+++ src/components/Layout/DefaultHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <div class="container py-3">
       <div class="flex justify-end space-x-5">
-        <div class="space-x-3">
+        <div class="">
           <button
             @click="locale = availableLocal"
             v-for="availableLocal in availableLocales"
@@ -12,6 +12,7 @@
           >
             {{ availableLocal }}
           </button>
+          <hr />
         </div>
 
         <button @click="toggle">
@@ -39,11 +40,23 @@
 
 <style scoped>
   .locale {
-    @apply text-gray-300 dark:text-gray-500 transitable;
+    @apply text-gray-300 dark:text-gray-500 transitable w-16;
 
     &:hover,
     &.active {
       @apply text-gray-500 dark:text-gray-300;
     }
+
+    &:nth-child(1).active ~ hr {
+      @apply ml-0;
+    }
+
+    &:nth-child(2).active ~ hr {
+      @apply ml-16;
+    }
+  }
+
+  hr {
+    @apply w-16 h-1 mt-1 bg-purple-300 border-none transitable;
   }
 </style>
